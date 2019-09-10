391fa58f045c0052edf1b808ca0139b37351bd46
pawelpopiel <55151589+pawelpopiel@users.noreply.github.com>
2019-09-10 18:27:03 +0200

    Wersja 1

---
+++ ConsoleAppEmerytura/ConsoleAppEmerytura/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEmerytura
+{
+    class Program
+    {
+        const int wiekEmerytalny = 67;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aplikacja EMERYTURA");
+
+            Console.WriteLine("Podaj imie: ");
+            string imie = Console.ReadLine();
+            Console.WriteLine($"Witaj {imie}");
+       
+            Console.Write("Podaj wiek: ");
+            int wiek = int.Parse(Console.ReadLine());
+            if (wiek < 67 )
+            {
+                Console.WriteLine($"Zostało Ci {wiekEmerytalny - wiek} lat do emerytury");
+            }
+            else
+            {
+                Console.WriteLine("Jesteś emerytem.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
